d9381ae6288e6b0bfbc911f235bb2d52f8e4f38d
Martin Kennedy <hurricos@gmail.com>
2018-06-01 17:48:32 -0400

    Updated install script, added autoftpd files and tweaks
    Added a configuration `LED_DISABLED` to `autoftp.defaults.conf` to
    allow to disable operation of LEDs (as this project is configured
    against the LEDs available on the gl-AR300M, and not necessarily on
    any other router.
    
    Added all of the required files, except for the `login.cfg` file,
    which should be placed at `/etc/autoftp/login.cfg`.
    
    Completed installation script.

--- install.sh
+++ install.sh
@@ -6,3 +6,26 @@ opkg update
 opkg install ncftp
 [ $? -ne 0 ] && echo "Failed to install required dependencies. Please check your internet connection or remaining flash space." && exit 1;
 
+mv udhcpc.user /etc/udhcpc.user \
+	&& echo "Moved udhcpc.user to /etc/udhcpc.user"
+
+mv dhcplogd /etc/autoftp/dhcplogd \
+	&& echo "Moved dhcplogd to /etc/autoftp/dhcplogd"
+
+mkdir /etc/autoftp/ \
+	&& echo "Created directory /etc/autoftp"
+
+mv autoftpd /etc/init.d/autoftpd \
+	&& echo "Moved autoftpd to /etc/init.d/autoftpd"
+
+mv autoftp /etc/autoftp/autoftp \
+   	&& echo "Moved autoftp to /etc/autoftp/autoftp"
+
+mv autoftp.defaults.conf /etc/autoftp/autoftp.defaults.conf \
+	&& echo "Moved autoftp.defaluts.conf to /etc/autoftp/autoftp.defaults.conf"
+
+/etc/init.d/autoftpd enable \
+	&& echo "Enabled autoftpd for next startup"
+
+echo "Don't forget to move your 'login.cfg' file to /etc/autoftp/login.cfg."
+echo "Successfully installed autoftpd."
